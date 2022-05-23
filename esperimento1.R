72c4bc7b1fc757ca76d709308cd77382aeb92b69
smeligrana <admin@example.com>
2022-05-23 21:41:49 +0200

    grafici dei modelli

--- esperimento1.R
+++ esperimento1.R
@@ -27,7 +27,8 @@ boolean_to_number <- function(x){
   as.integer(as.logical(x))
 }
 
-features <- c('num_valutazioni', 'qta_min', 'valutazioni_positive', 'dif_prezzo', 'dif_prezzo_sped', 'dif_prezzo_tot', 'stelle', 'vend_amazon', 'buy_box')
+features <- c('num_valutazioni', 'qta_min', #'valutazioni_positive', 
+              'dif_prezzo', 'dif_prezzo_sped', 'dif_prezzo_tot', 'stelle', 'vend_amazon', 'delta_consegna', 'buy_box')
 
 #funzione per estrazione dataset not buy box
 extract_not_buy_box <- function(csv){
@@ -36,18 +37,21 @@ extract_not_buy_box <- function(csv){
   data_not_buy_box <- NULL
   for (x in dates) {
     a <- csv[csv$buy_box==0 & csv$timestamp==x, features]
-    randomRows <- sample(7, 2)
+    randomRows <- sample(7, 3)
     data_not_buy_box <- rbind(data_not_buy_box, a[randomRows[1],])
     data_not_buy_box <- rbind(data_not_buy_box, a[randomRows[2],])
+    data_not_buy_box <- rbind(data_not_buy_box, a[randomRows[3],])
   }
   return(data_not_buy_box)
 }
 
+setwd("C:/Users/Sergio/Desktop/uni/tesi/R/")
+
 # carico il csv degli esperimenti
-csv_caffe <- read.csv(file='C:/Users/Sergio/Desktop/uni/tesi/R/Alimentari - Capsule caffè - Foglio1.csv')
-csv_agenda <- read.csv(file='C:/Users/Sergio/Desktop/uni/tesi/R/Ufficio - Agenda moleskine - Foglio1.csv')
-csv_lampadine <- read.csv(file='C:/Users/Sergio/Desktop/uni/tesi/R/Illuminazione - Lampadine Philips - Foglio1.csv')
-csv_sport <- read.csv(file='C:/Users/Sergio/Desktop/uni/tesi/R/Sport e tempo libero - Smartwatch - Xiaomi Mi Smart Band 6 - Foglio1.csv')
+csv_caffe <- read.csv(file='Alimentari - Capsule caffè - Foglio1.csv')
+csv_agenda <- read.csv(file='Ufficio - Agenda moleskine - Foglio1.csv')
+csv_lampadine <- read.csv(file='Illuminazione - Lampadine Philips - Foglio1.csv')
+csv_sport <- read.csv(file='Sport e tempo libero - Smartwatch - Xiaomi Mi Smart Band 6 - Foglio1.csv')
 
 data_not_buy_box = extract_not_buy_box(csv_caffe)
 data_not_buy_box <- rbind(data_not_buy_box, extract_not_buy_box(csv_agenda))
@@ -66,36 +70,41 @@ data_buy_box <- rbind(data_buy_box_caffe, data_buy_box_agenda, data_buy_box_lamp
 data <- rbind(data_buy_box, data_not_buy_box)
 
 # converto im valori numerici le colonne che contengono numeri sotto forma di stringa
-data$dif_prezzo <- unlist(lapply(data[4], string_to_number))
-data$dif_prezzo_sped <- unlist(lapply(data[5], string_to_number))
-data$dif_prezzo_tot <- unlist(lapply(data[6], string_to_number))
-data$stelle <- unlist(lapply(data[7], string_to_number))
-data$vend_amazon <- unlist(lapply(data[8], boolean_to_number))
+#data$dif_prezzo <- unlist(lapply(data[3], string_to_number))
+data$dif_prezzo <- string_to_number(data$dif_prezzo)
+data$dif_prezzo_sped <- string_to_number(data$dif_prezzo_sped)
+data$dif_prezzo_tot <- string_to_number(data$dif_prezzo_tot)
+data$stelle <-string_to_number(data$stelle)
+#data$vend_amazon <- string_to_number(data$vend_amazon)
+data$delta_consegna <- string_to_number(data$delta_consegna)
 
 # normalizzo i dati
-data$num_valutazioni <- unlist(lapply(data[1], normalize))
-data$valutazioni_positive <- unlist(lapply(data[3], normalize))
-data$dif_prezzo <- unlist(lapply(data[4], normalize))
-data$dif_prezzo_sped <- unlist(lapply(data[5], normalize))
-data$dif_prezzo_tot <- unlist(lapply(data[6], normalize))
+data$num_valutazioni <- normalize(data$num_valutazioni)
+#data$valutazioni_positive <- unlist(lapply(data[3], normalize))
+data$dif_prezzo <- normalize(data$dif_prezzo)
+data$dif_prezzo_sped <-normalize(data$dif_prezzo_sped)
+data$dif_prezzo_tot <- normalize(data$dif_prezzo_tot)
+data$stelle <- normalize(data$stelle)
+data$vend_amazon <-normalize(data$vend_amazon)
+data$delta_consegna <- normalize(data$delta_consegna)
 
 
 
 # divido i dati nel sotto insieme di training e di test
 ind <- sample(2, nrow(data), replace=TRUE, prob=c(0.60, 0.40))
 
-data.training <- data[ind==1, 1:8]
-data.trainLabels <- data[ind==1,9]
-data.test <- data[ind==2, 1:8]
-data.testLabels <- data[ind==2, 9]
+#data.training <- data[ind==1, 1:8]
+#data.trainLabels <- data[ind==1,9]
+#data.test <- data[ind==2, 1:8]
+#data.testLabels <- data[ind==2, 9]
 
 # elaboro il modello utilizzando knn k-Nearest Neighbour Classification
-data_pred <- knn(train = data.training, test = data.test, cl = data.trainLabels, k=3)
+#data_pred <- knn(train = data.training, test = data.test, cl = data.trainLabels, k=3)
 
-merge <- data.frame(data_pred, data.testLabels)
+#merge <- data.frame(data_pred, data.testLabels)
 
 # Verifica accuratezza modello
-mean(data_pred == data.testLabels)
+#mean(data_pred == data.testLabels)
 
 #names(merge) <- c("Predicted", "Observed")
 #merge
@@ -131,11 +140,18 @@ mean(predicted.randomforest == data.test$buy_box)
 
 importance(randomforest_model)
 
+varImpPlot(randomforest_model, sort=T, n.var= 8, main= "", pch=16)
+
 
 #SVM
-svm.model <- svm(buy_box ~ ., data = data.training, kernel = "radial", gamma = 1, cost = 1)
-svm.model
+svm.model <- svm(buy_box ~ num_valutazioni+vend_amazon, data = data.training, kernel = "linear", gamma = 1, cost = 1)
+summary(svm.model)
+
 
 svm.pred <- predict(svm.model, data.test)
+plot(svm.model , data.training[, c('num_valutazioni','vend_amazon','buy_box')])
+plot( data.training$num_valutazioni, data.training$vend_amazon, lwd=3 )
 
 mean(svm.pred ==data.test$buy_box)
+
+
